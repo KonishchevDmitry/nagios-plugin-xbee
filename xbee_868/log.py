3a302df0fb4249333bb6d7145efd45f652e1f5f9
Dmitry Konishchev <konishchev@gmail.com>
2013-06-25 15:52:53 +0400

    Refactor xbee_868.log

--- xbee_868/log.py
+++ xbee_868/log.py
@@ -1,38 +1,11 @@
-# TODO
-"""
-Provides a few tools that do most of routine actions which you have to do when
-you work with Python's logging module.
-"""
+"""Configures logging."""
 
-from __future__ import print_function # Suppresses Pyflakes errors
+from __future__ import unicode_literals
 
 import logging
-import sys
 
 
-class OutputHandler(logging.Handler):
-    """
-    Log handler that logs debug and info messages to stdout and all other
-    messages to stderr.
-    """
-
-    def __init__(self, *args, **kwargs):
-        logging.Handler.__init__(self, *args, **kwargs)
-
-
-    def emit(self, record):
-        self.acquire()
-
-        try:
-            stream = sys.stdout if record.levelno <= logging.INFO else sys.stderr
-            print(self.format(record), file = stream)
-        except:
-            self.handleError(record)
-        finally:
-            self.release()
-
-
-def setup(name = None, debug_mode = False, filter = None, max_log_name_length = 16, level = None):
+def setup(debug_mode=False):
     """Sets up the logging."""
 
     logging.addLevelName(logging.DEBUG,   "D")
@@ -40,20 +13,14 @@ def setup(name = None, debug_mode = False, filter = None, max_log_name_length = 
     logging.addLevelName(logging.WARNING, "W")
     logging.addLevelName(logging.ERROR,   "E")
 
-    log = logging.getLogger(name)
-
+    log = logging.getLogger(__name__.split(".")[0])
     log.setLevel(logging.DEBUG if debug_mode else logging.INFO)
-    if level is not None:
-        log.setLevel(level)
 
-    format = ""
+    log_format = "%(asctime)s.%(msecs)03d"
     if debug_mode:
-        format += "%(asctime)s.%(msecs)03d (%(filename)12.12s:%(lineno)04d) [%(name){0}.{0}s]: ".format(max_log_name_length)
-    format += "%(levelname)s: %(message)s"
+        log_format += " (%(filename)12.12s:%(lineno)04d)"
+    log_format += ": %(levelname)s: %(message)s"
 
-    handler = OutputHandler()
-    handler.setFormatter(logging.Formatter(format, "%Y.%m.%d %H:%M:%S"))
-    if filter is not None:
-        handler.addFilter(filter)
-
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter(log_format, "%Y.%m.%d %H:%M:%S"))
     log.addHandler(handler)
