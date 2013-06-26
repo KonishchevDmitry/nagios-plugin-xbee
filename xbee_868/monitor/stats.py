50bcb141d8493ecff4cb84420b00b4e56eec56a5
Dmitry Konishchev <konishchev@gmail.com>
2013-06-26 16:27:11 +0400

    Update xbee_868.modified.stats module

--- xbee_868/monitor/stats.py
+++ xbee_868/monitor/stats.py
@@ -1,5 +1,30 @@
+"""Collects all monitor's statistics."""
+
 from __future__ import unicode_literals
 
-# TODO
-def get():
-    return { "test_key": "test_value" }
+import time
+
+from xbee_868.common.core import Error
+
+_MONITOR_START_TIME = None
+"""The monitor service start time."""
+
+
+def monitor_started():
+    """Called on the monitor start."""
+
+    global _MONITOR_START_TIME
+
+    if _MONITOR_START_TIME is not None:
+        raise Error("The monitor is already started.")
+
+    _MONITOR_START_TIME = time.time()
+
+
+def uptime():
+    """Returns current monitor uptime."""
+
+    if _MONITOR_START_TIME is None:
+        raise Error("The monitor is not started.")
+
+    return int(time.time() - _MONITOR_START_TIME)
