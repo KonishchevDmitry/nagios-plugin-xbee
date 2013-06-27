2c2ed7eadb79975cff93616f6cf69600bdcb5eaf
Dmitry Konishchev <konishchev@gmail.com>
2013-06-27 13:12:50 +0400

    Change server socket path

--- xbee_868/common/constants.py
+++ xbee_868/common/constants.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 BUFSIZE = 4 * 1024
 """I/O buffer size."""
 
-SERVER_SOCKET_PATH = "/var/run/xbee-868/monitor.socket"
+SERVER_SOCKET_PATH = "/var/run/xbee-868-monitor"
 """Path to the server socket."""
 
 IPC_TIMEOUT = 10
